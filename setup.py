5b20f2f78f94a4a8076225f27e8f7e669aecda8a
Rafael Vescovi <17483715+ravescovi@users.noreply.github.com>
2023-09-13 17:16:50 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -1,50 +1,35 @@
-import os
 from setuptools import setup, find_packages
 
-
-install_requires = ["setuptools"]
-# with open('requirements.txt') as reqs:
-#     for line in reqs.readlines():
-#         req = line.strip()
-#         if not req or req.startswith('#'):
-#             continue
-#         install_requires.append(req)
-
-
-package_name = 'pf400_driver'
+install_requires = []
 
 setup(
-    name='pf400_driver',
-    version='0.0.1',
+    name="pf400_driver",
+    version="0.0.1",
     packages=find_packages(),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=[],
     install_requires=install_requires,
     zip_safe=True,
     python_requires=">=3.8",
-    maintainer='Doga Ozgulbas and Alan Wang',
-    maintainer_email='dozgulbas@anl.gov',
-    description='Driver for the PF400 robot arm',
-    url='https://github.com/AD-SDL/PF400_driver.git', 
-    license='MIT License',
-    entry_points={ 
-        'console_scripts': [
-             'pf400_driver = pf400_driver.pf400_driver:main_null',
-             'tcp_driver = pf400_driver.tcp_driver:main_null',
-             'pf400_camera_driver =  pf400_driver.pf400_camera_driver:main_null'
+    maintainer="Doga Ozgulbas and Alan Wang",
+    maintainer_email="dozgulbas@anl.gov",
+    description="Driver for the PF400 robot arm",
+    url="https://github.com/AD-SDL/pf400_module.git",
+    license="MIT License",
+    entry_points={
+        "console_scripts": [
+            "pf400_driver = pf400_driver.pf400_driver:main_null",
+            "tcp_driver = pf400_driver.tcp_driver:main_null",
+            "pf400_camera_driver =  pf400_driver.pf400_camera_driver:main_null",
         ]
     },
     classifiers=[
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX',
-        'Operating System :: MacOS :: MacOS X',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )

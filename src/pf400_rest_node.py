b3e58a5546487c820e56c972b87365e8415183fd
Dozgulbas <dozgulbas@anl.gov>
2025-03-18 15:27:46 -0700

    Working on the actions

--- src/pf400_rest_node.py
+++ src/pf400_rest_node.py
@@ -2,17 +2,16 @@
 """The server for the PF400 robot that takes incoming WEI flow requests from the experiment application"""
 
 import datetime
-import traceback
 from time import sleep
-from typing import List, Optional, Union, Any
+from typing import Any, List, Optional, Union
 
+from madsci.client.resource_client import ResourceClient
 from madsci.common.types.action_types import ActionFailed, ActionSucceeded
-from madsci.common.types.node_types import RestNodeConfig
 from madsci.common.types.location_types import Location
+from madsci.common.types.node_types import RestNodeConfig
 from madsci.node_module.abstract_node_module import action
 from madsci.node_module.rest_node_module import RestNode
 from pf400_interface.pf400 import PF400
-from pf400_interface.pf400_errors import ConnectionException
 from typing_extensions import Annotated
 
 
@@ -38,10 +37,9 @@ class PF400Node(RestNode):
         """Called to (re)initialize the node. Should be used to open connections to devices or initialize any other resources."""
         try:
             self.logger.log("Node initializing...")
-            self.pf400_interface = PF400(
-                host=self.config_model.ip
-            )
+            self.pf400_interface = PF400(host=self.config_model.ip)
             self.pf400_interface.initialize_robot()
+            self.resource_client = ResourceClient(url="http://testserver")
         except Exception as err:
             self.logger.log_error(f"Error starting the PF400 Node: {err}")
             self.startup_has_run = False
@@ -107,33 +105,39 @@ class PF400Node(RestNode):
         target_approach: Annotated[dict[str, Any], "Location to approach from"] = None,
         source_plate_rotation: Annotated[
             str, "Orientation of the plate at the source, wide or narrow"
-        ]= "",
+        ] = "",
         target_plate_rotation: Annotated[
             str, "Final orientation of the plate at the target, wide or narrow"
-        ]= "",
+        ] = "",
     ):
         """A doc string, but not the actual description of the action."""
         try:
             source = Location.model_validate(source)
             target = Location.model_validate(target)
-            source_approach = Location.model_validate(source_approach) if source_approach else None
-            target_approach = Location.model_validate(target_approach) if target_approach else None
+            source_approach = (
+                Location.model_validate(source_approach) if source_approach else None
+            )
+            target_approach = (
+                Location.model_validate(target_approach) if target_approach else None
+            )
         except Exception as e:
             return ActionFailed(errors=f"Invalid location data: {e}")
-        
+        # resource = self.resource_client.get_resource(resource_id=source.resource_id)
+        popped_plate, resource = self.resource_client.pop(source.resource_id)
         self.pf400_interface.transfer(
-            source_loc=source,
-            target_loc=target,
-            source_approach=source_approach,
-            target_approach= target_approach,
+            source_loc=source.look_up,
+            target_loc=target.look_up,
+            source_approach=source_approach.look_up,
+            target_approach=target_approach.look_up,
             source_plate_rotation=source_plate_rotation,
             target_plate_rotation=target_plate_rotation,
         )
+        self.resource_client.push(target.resource_id, popped_plate)
 
         # if result:
         return ActionSucceeded()
         # return ActionFailed(
-            # errors=f"`run_command` returned '{result}'. Expected 'True'."
+        # errors=f"`run_command` returned '{result}'. Expected 'True'."
         # )
 
     def pause(self) -> None:
@@ -175,189 +179,6 @@ class PF400Node(RestNode):
         self.node_status.cancelled = True
         self.logger.log("Node cancelled.")
         return True
-
-
-# ------------------------------------------------------------------------------#
-rest_module = RESTModule(
-    name="pf400_node",
-    version="0.0.1",
-    description="A node to control the pf400 plate moving robot",
-    model="pf400",
-)
-rest_module.arg_parser.add_argument(
-    "--pf400_ip", type=str, help="pf400 ip value", default="146.137.240.35"
-)
-rest_module.arg_parser.add_argument(
-    "--pf400_port", type=int, help="pf400 port value", default=10100
-)
-
-
-@rest_module.startup()
-def pf400_startup(state: State):
-    """Example startup handler."""
-    state.pf400 = None
-    state.action_start = None
-    try:
-        state.pf400 = PF400(state.pf400_ip, state.pf400_port)
-        state.pf400.initialize_robot()
-    except Exception:
-        traceback.print_exc()
-
-    else:
-        print("PF400 online")
-
-    # Do any cleanup here
-
-
-def check_state(state: State):
-    """Updates the PF400 state
-
-    Parameters:
-    -----------
-        None
-    Returns
-    -------
-        None
-    """
-    # TODO: Simplify this function
-
-    try_connect = False
-    err = False
-
-    try:
-        movement_state = state.pf400.movement_state
-
-    except UnboundLocalError as local_var_err:
-        traceback.print_exc()
-        err = local_var_err
-
-    except AttributeError as attribute_err:
-        traceback.print_exc()
-        err = attribute_err
-        try_connect = True
-
-    except Exception as general_err:
-        traceback.print_exc()
-        err = general_err
-
-    finally:
-        if try_connect:
-            state.status[ModuleStatus.READY] = False
-            state.status[ModuleStatus.ERROR] = True
-            try:
-                state.pf400 = PF400(state.pf400_ip, state.pf400_port)
-                state.pf400.initialize_robot()
-                state.status[ModuleStatus.READY] = True
-                state.status[ModuleStatus.ERROR] = False
-
-            except ConnectionException as error_msg:
-                state.status[ModuleStatus.READY] = False
-                state.status[ModuleStatus.ERROR] = True
-                print(error_msg)
-
-            except Exception as err:
-                state.status[ModuleStatus.READY] = False
-                state.status[ModuleStatus.ERROR] = True
-                print(err)
-            else:
-                print("PF400 online")
-
-    if err:
-        state.status[ModuleStatus.READY] = False
-        state.status[ModuleStatus.ERROR] = True
-        return
-
-    # Check if robot wasn't attached to the software after recovering from Power Off state
-    if state.pf400.attach_state == "-1":
-        state.status[ModuleStatus.READY] = False
-        state.status[ModuleStatus.ERROR] = True
-        state.pf400.force_initialize_robot()
-
-    # Publishing robot warning messages if the job wasn't completed successfully
-    if (
-        state.pf400.robot_warning.upper() != "CLEAR"
-        and len(state.pf400.robot_warning) > 0
-    ):
-        state.status[ModuleStatus.READY] = False
-        state.status[ModuleStatus.ERROR] = True
-        state.pf400.robot_warning = "CLEAR"
-
-    # Checking real robot state parameters and publishing the current state
-    if movement_state == 0:
-        state.status[ModuleStatus.READY] = False
-        state.status[ModuleStatus.ERROR] = True
-        state.pf400.force_initialize_robot()
-
-    elif state.pf400.robot_state == "ERROR" or state.status == ModuleStatus.ERROR:
-        state.status[ModuleStatus.READY] = False
-        state.status[ModuleStatus.ERROR] = True
-    elif (
-        movement_state >= 1 and state.status == ModuleStatus.BUSY
-    ) or movement_state >= 2:
-        state.status[ModuleStatus.READY] = False
-        state.status[ModuleStatus.BUSY] = True
-
-
-@rest_module.state_handler()
-def state(state: State):
-    """Returns the current state of the Pf400 module"""
-
-    if (
-        not (state.status[ModuleStatus.BUSY])
-        and not (state.status[ModuleStatus.INIT])
-        or (
-            state.action_start
-            and (
-                datetime.datetime.now() - state.action_start > datetime.timedelta(0, 2)
-            )
-        )
-    ):
-        check_state(state)
-    return JSONResponse(content={"status": state.status, "error": state.error})
-
-
-@rest_module.action(
-    name="transfer", description="Transfer a plate from one location to another"
-)
-def transfer(
-    state: State,
-    action: ActionRequest,
-    source: Annotated[List[float], "Location to pick a plate from"],
-    target: Annotated[List[float], "Location to place a plate to"],
-    source_approach: Annotated[List[float], "Location to approach from"] = None,
-    target_approach: Annotated[List[float], "Location to approach from"] = None,
-    # source_approach=None,
-    # target_approach=None,
-    source_plate_rotation: Annotated[
-        str, "Orientation of the plate at the source, wide or narrow"
-    ] = "",
-    target_plate_rotation: Annotated[
-        str, "Final orientation of the plate at the target, wide or narrow"
-    ] = "",
-) -> StepResponse:
-    """Transfer a plate from one location to another"""
-    sleep(0.3)
-    err = None
-    if len(source) != 6:
-        err = True
-        msg = "Position 1 should be six joint angles length. Canceling the job!"
-    elif len(target) != 6:
-        err = True
-        msg = "Position 2 should be six joint angles length. Canceling the job!"
-    if err:
-        return StepResponse.step_failed(error=msg)
-    sleep(0.3)
-    state.action_start = datetime.datetime.now()
-    state.pf400.transfer(
-        source=source,
-        target=target,
-        source_approach=source_approach,
-        target_approach=target_approach,
-        source_plate_rotation=source_plate_rotation,
-        target_plate_rotation=target_plate_rotation,
-    )
-    state.action_start = None
-    return StepResponse.step_succeeded()
 
 
 @rest_module.action(

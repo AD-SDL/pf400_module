129ae2c9c105ac3d45bad2feb17359dbac7fd921
Mark Hereld <mhereld@gmail.com>
2025-09-20 12:58:03 -0500

    add explanation of units in pf400_kinematics.py
    Joint and Cartesian coordinates are described, and in particular, units of the values are made explicit.

--- src/pf400_interface/pf400_kinematics.py
+++ src/pf400_interface/pf400_kinematics.py
@@ -5,13 +5,31 @@ import math
 
 class KINEMATICS:
     """Class for calculating the forward and inverse kinematics of the PF400 robot arm."""
+    """
+    Joint coordinates, list of length 6 for PF400 with horizontal rail,
+      rail positions units are mm, angles in degrees increasing CCW looking down (-z):
+        0  vertical rail position in mm, +z up, range [ 0, 1500 ] mm (for the 1.5m v_rail)
+        1  angle of shoulder joint (v_rail to upper_arm), 0 deg perp v_rail, range: [-93, 93] deg
+        2  angle of elbow joint (upper_arm to forearm), 0 deg under upper_arm, range: [-168, 168] deg
+        3  angle of wrist joint (forearm to end_effector), 0 deg extends forearm, range: [-960, 960] deg
+        4  the separation between plate-handling gripper fingers, range: [ ???, ??? ] mm
+        5  horizontal rail position in mm, +x perp v_rail, range: [-1000,1000] (for the 2m h_rail)
+    
+    Cartesian coordinates, list of length 6:
+        0  X       increasing away from the face of the v_rail, parallel to the h_rail
+        1  Y       increasing to the left when facing in the +x direction
+        2  Z       increasing up along the v_rail
+        3  yaw     angle of gripper fingers to +x axis
+        4  pitch   fixed at  90 deg
+        5  roll    fixed at 180 deg
+    """
 
     def __init__(self) -> None:
         """Constructor for the KINEMATICS class."""
-        # Robot joint lengths
-        self.shoulder_length = 302
-        self.elbow_length = 289
-        self.end_effector_length = 162
+        # Robot arm segment lengths (in millimeters)
+        self.shoulder_length     = 302 # mm    (perhaps rename to upper_arm, as shoulder is a joint)
+        self.elbow_length        = 289 # mm    (perhaps rename to forearm, as elbo is a joint)
+        self.end_effector_length = 162 # mm    (???does this length terminate at the Tool Center Point?)
 
     def forward_kinematics(
         self, joint_states: list[float]
@@ -21,7 +39,7 @@ class KINEMATICS:
         Parameters:
             - joint_states : 6 joint states of the target location
         Return:
-            - cartesian_coordinates: Returns the calculated cartesian coordinates of the given joint states
+            - cartesian_coordinates: Returns the calculated cartesian coordinates of the given joint states (in millimeters)
             - phi: Phi angle in degrees to be used for inverse kinematics
             - joint_state[5]: The rail length. Needs to be subtracted from x axis if calculated coordinates will be fed into inverse kinematics
         """
